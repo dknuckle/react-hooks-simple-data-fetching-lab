d59fa818dea4d524db48ab713bd23db4cf0282d5
Dalton Knuckles <djck244@gmail.com>
2023-06-12 18:34:11 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [picOfDogs, setPicOfDogs] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setPicOfDogs(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return <img alt="A Random Dog" src={picOfDogs} />;
+}
+
+export default App;
